ea5c7967eb6aa94d4c36b8f4fc8f5b91cfbfc691
Kiseleva Natalia <kiselyov-natalya@mail.ru>
2023-03-25 21:59:49 +0300

    remove imports

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,12 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
